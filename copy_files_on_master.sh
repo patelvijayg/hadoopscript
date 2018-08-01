36be2aa2a81000b427d4d96ee913868eb04a77da
vijay limbasiya <vijay.limbasiya@netcracker.com>
2018-08-01 15:58:28 +0530

    added few alias

--- copy_files_on_master.sh
+++ copy_files_on_master.sh
@@ -5,9 +5,10 @@ hadoopfilesdir=$SCRIPT_DIR/hadoopfiles
 sparkfilesdir=$SCRIPT_DIR/sparkfiles
 echo "copying hadoop files from  $hadoopfiles to master node $HADOOP_HOME/etc/hadoop/"
 
-cp -Rf $hadoopfilesdir/*.xml $HADOOP_HOME/etc/hadoop/
-cp -Rf $hadoopfilesdir/*.sh $HADOOP_HOME/etc/hadoop/
+cp -Rf $hadoopfilesdir/* $HADOOP_HOME/etc/hadoop/
+#cp -Rf $hadoopfilesdir/*.xml $HADOOP_HOME/etc/hadoop/
+#cp -Rf $hadoopfilesdir/*.sh $HADOOP_HOME/etc/hadoop/
 
-cp -Rf $sparkfilesdir/*.* $SPARK_CONF_DIR/
+cp -Rf $sparkfilesdir/* $SPARK_CONF_DIR/
 
 echo "Setup is successfully"
