85c38aecfbf6c90737bf0cdeb908ce71c74a475d
vijay limbasiya <vijay.limbasiya@netcracker.com>
2018-08-01 13:46:52 +0530

    updated scripts by correcting script files

--- cleanfs.sh
+++ cleanfs.sh
@@ -1,29 +1,13 @@
 #!/bin/bash
 echo "deleting $HADOOP_HOME/infra/namenode"
-if [ -d $HADOOP_HOME/infra/namenode/ ]
-then
-	rm -rf $HADOOP_HOME/infra/namenode/*.*
-fi
 
-echo "deleting $HADOOP_HOME/infra/datanode"
-if [ -d $HADOOP_HOME/infra/datanode/ ] 
-then
-	rm -rf $HADOOP_HOME/infra/datanode/*.*
-fi
-
-echo "deleting $HADOOP_HOME/logs"
-if [ -d $HADOOP_HOME/logs ]
-	rm -rf $HADOOP_HOME/logs/*.*
-else
-	mkdir -p $HADOOP_HOME/logs
-fi
-
-echo "deleting $SPARK_HOME/logs"
-if [ -d $SPARK_HOME/logs ]
-	rm -rf $SPARK_HOME/logs/*.*
-else
-	mkdir -p $SPARK_HOME/logs
-fi
+[ -d $HADOOP_HOME/infra/namenode ] && rm -rf $HADOOP_HOME/infra/namenode
+[ -d $HADOOP_HOME/infra/datanode ] && rm -rf $HADOOP_HOME/infra/datanode
+[ -d $HADOOP_HOME/logs ] && rm -rf $HADOOP_HOME/logs
+[ -d $SPARK_HOME/logs ] && rm -rf $SPARK_HOME/logs
+mkdir -p $HADOOP_HOME/infra/namenode
+mkdir -p $HADOOP_HOME/infra/datanode
+mkdir -p $HADOOP_HOME/logs
+mkdir -p $SPARK_HOME/logs
 
 echo "deleted $HADOOP_HOME/hadoop/infra and $HADOOP_PREFIX/logs"
-
--- unittest.sh
+++ unittest.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 onefile=uPM_all_metrics-20180730080000-20180730081500.csv
+dir1=unittest
 cp $SCRIPT_DIR/datafiles/dataset1.7z /tmp/
 cd /tmp
 7za x dataset1.7z -y
-hdfs dfs -mkdir /data1
-hdfs dfs -put /tmp/dataset1/$onefile /data1
-hdfs dfs -cat /data1/$onefile
+hdfs dfs -rm /$dir1
+hdfs dfs -mkdir /$dir1
+hdfs dfs -put /tmp/dataset1/$onefile /$dir1
+hdfs dfs -cat /$dir1/$onefile
