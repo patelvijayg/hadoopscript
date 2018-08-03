ec04305c8132ac677ca6e264777def4f376d3779
vijay limbasiya <vijay.limbasiya@netcracker.com>
2018-08-03 17:07:19 +0530

    added new alias

--- syncnode.sh
+++ syncnode.sh
@@ -2,12 +2,12 @@
 #Enter the number of slave node so it will clean one by one all the nodes
 for slave in $(cat $HADOOP_CONF_DIR/slaves); do 
    echo "syncing hadoop and scripts files with $slave"
-   rsync -avzhe ssh $HADOOP_CONF_DIR/*.* vagrant@$slave:$HADOOP_CONF_DIR/
-   rsync -avzhe ssh $SCRIPT_DIR/*.* vagrant@$slave:$SCRIPT_DIR/
+   rsync -avzhe ssh $HADOOP_CONF_DIR/*.* vagrant@$slave:$HADOOP_CONF_DIR/ --delete
+   rsync -avzhe ssh $SCRIPT_DIR/ vagrant@$slave:$SCRIPT_DIR/ --delete
 done
 for slave in $(cat $SPARK_CONF_DIR/slaves); do 
    echo "syncing spark with $slave"
-   rsync -avzhe ssh $SPARK_CONF_DIR/*.* vagrant@$slave:$SPARK_CONF_DIR/
+   rsync -avzhe ssh $SPARK_CONF_DIR/*.* vagrant@$slave:$SPARK_CONF_DIR/ --delete
 done
 echo "sync process completed."
 
