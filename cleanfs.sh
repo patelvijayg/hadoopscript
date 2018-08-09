b70a47ba2671662d77d899cfa8d01ba34f1253cd
vijay limbasiya <vijay.limbasiya@netcracker.com>
2018-08-09 19:40:25 +0530

    order the delete and create in temp folder

--- cleanfs.sh
+++ cleanfs.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 #you can add the folder wish to delete during reset filesystem
-listofdir=($HADOOP_HOME/infra/namenode $HADOOP_HOME/infra/datanode $HADOOP_HOME/logs $SPARK_HOME/logs /tmp/sparkevent)
+listofdir=($(echo /tmp/spark*))
+for i in "${listofdir[@]}"
+do
+[[ -d $i ]] && rm -rf $i
+	echo "$i deleted ........" 
+done
+
+listofdir=($HADOOP_HOME/infra/namenode $HADOOP_HOME/infra/datanode $HADOOP_HOME/logs $SPARK_HOME/logs /tmp/sparkevent/eventLog)
 for i in "${listofdir[@]}"
 do
 [[ -d $i ]] && rm -rf $i
@@ -9,11 +16,6 @@ mkdir -p $i
 echo "$i created ........" 
 done
 
-listofdir=($(echo /tmp/spark*))
-for i in "${listofdir[@]}"
-do
-[[ -d $i ]] && rm -rf $i
-	echo "$i deleted ........" 
-done
+
 
 echo "Completed the operation"
