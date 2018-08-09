8506210c0be341d9ce1e60a3a6c54d7bdca28161
vijay limbasiya <vijay.limbasiya@netcracker.com>
2018-08-09 18:24:09 +0530

    added some python program

---
+++ samples/hdfs_order_take.py
@@ -0,0 +1,8 @@
+from __future__ import print_function
+from pyspark.sql import SparkSession
+if __name__ == "__main__":
+	spark = SparkSession.builder.appName("unittesting1").getOrCreate()
+	df1=spark.read.json ("/public/retail_db_json/orders")
+	df1.show (2, False)
+	spark.stop()
+	print("now program has been stopped")
